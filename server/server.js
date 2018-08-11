96aab29f13df289e64d54cb3892336490c60a4b0
Leandro Pross <leandropross@gmail.com>
2018-08-11 19:32:03 -0300

    Finished exercise - delete not tested

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -22,6 +22,67 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+
+app.get('/lions/:id', function (req, res) {
+    let lion = getLion(req.params.id);
+
+    if (!lion) {
+        resourceNotFound(res);
+        return;
+    }    
+
+    res.json(lion);
+});
+
+app.post('/lions', function (req, res) {
+    /*if (err) {
+        res.statusCode = 500;
+        res.json({ error: "Alto error ameo" })
+    }*/
+
+    let lion = req.body;
+    lion.id = ++id;
+
+    lions.push(lion);
+
+    res.json(lion);
+});
+
+app.get('/lions', function (req, res) {
+    res.json(lions);
+});
+
+app.delete('/lions/:id', function (req, res) {
+    let lion = getLion(req.params.id);
+
+    if (!lion) {
+        resourceNotFound(res);
+        return;
+    }
+
+    lions.splice(lions.findIndex(lion = req.params.id));
+});
+
+function resourceNotFound (response) {
+    response.statusCode = 500;
+    response.json({error: "The requested resource doesn't exist"});
+}
+
+function getLion(id) {
+    var result = lions.find(lion => {
+        return lion.id == id
+    })
+
+    return result;
+};
+
+function removeLion(lion) {
+    const index = array.indexOf(lion);
+
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
 
 app.listen(3000);
 console.log('on port 3000');
