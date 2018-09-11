21c0fdf97df1da570577a7584b228aa8bbdfa579
Leandro Pross <leandropross@gmail.com>
2018-09-11 00:39:17 -0300

    Exercise 6 done

--- server/api/api.js
+++ server/api/api.js
@@ -1,12 +1,15 @@
 var router = require('express').Router();
+var usersRouter = require('./user/userRoutes');
+var categoriesRouter = require('./category/categoryRoutes');
+var postsRouter = require('./post/postRoutes');
 
 // api router will mount other routers
 // for all our resources. Each resource directory
 // has a resourceRoutes.js file with the router ready to go,
 // require them and mount them to their respective routes below
 
-router.use('/users', /* require the router*/);
-router.use('/categories', /* require the router*/);
-router.use('/posts', /* require the router*/);
+router.use('/users', usersRouter);
+router.use('/categories', categoriesRouter);
+router.use('/posts', postsRouter);
 
 module.exports = router;
